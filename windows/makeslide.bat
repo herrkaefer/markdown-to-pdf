30827f5745440cbdcba36faa39b2b517edcbc2dd
herrkaefer <gloolar@gmail.com>
2014-07-15 13:11:47 +0800

    edit bat

--- windows/makeslide.bat
+++ windows/makeslide.bat
@@ -1,2 +1,2 @@
-pandoc %1 -o %~n1.pdf -t beamer --latex-engine=xelatex --template=beamer_template_win.tex -V theme=Warsaw
+pandoc %1 -o %~n1.pdf -t beamer --latex-engine=xelatex --template=%~dp0..\template\beamer_template.tex -V theme=Warsaw
 
