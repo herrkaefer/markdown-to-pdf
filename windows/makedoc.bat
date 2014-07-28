dce7b3ddb7c7298f46f5fee6c1cd677c1faed9d9
herrkaefer <gloolar@gmail.com>
2014-07-28 10:25:16 +0800

    add usage prompt

--- windows/makedoc.bat
+++ windows/makedoc.bat
@@ -1,1 +1,9 @@
+@echo off
+if [%1] == [] GOTO END
+
+@echo on
 pandoc %1 -o %~n1.pdf --latex-engine=xelatex --template=%~dp0..\template\latex_template.tex -N  -V documentclass=article -V fontsize=10pt --standalone --highlight-style tango
+
+:END
+echo No input file.
+echo usage: makedoc mdfile
--- windows/makeslide.bat
+++ windows/makeslide.bat
@@ -1,2 +1,9 @@
+@echo off
+if [%1] == [] GOTO END
+
+@echo on
 pandoc %1 -o %~n1.pdf -t beamer --standalone --latex-engine=xelatex --template=%~dp0..\template\beamer_template.tex -V theme=Warsaw
 
+:END
+echo No input file.
+echo usage: makedoc mdfile
