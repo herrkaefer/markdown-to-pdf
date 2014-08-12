7c17012c20824a147f557c2b6b93a576465212de
herrkaefer <gloolar@gmail.com>
2014-08-12 10:48:13 +0800

    modify windows scripts

--- windows/makedoc.bat
+++ windows/makedoc.bat
@@ -4,6 +4,9 @@ if [%1] == [] GOTO END
 @echo on
 pandoc %1 -o %~n1.pdf --latex-engine=xelatex --template=%~dp0..\template\latex_template.tex -N  -V documentclass=article -V fontsize=10pt --standalone --highlight-style tango
 
+@echo off
+Exit /B
+
 :END
 echo No input file.
 echo usage: makedoc mdfile
--- windows/makeslide.bat
+++ windows/makeslide.bat
@@ -4,6 +4,9 @@ if [%1] == [] GOTO END
 @echo on
 pandoc %1 -o %~n1.pdf -t beamer --standalone --latex-engine=xelatex --template=%~dp0..\template\beamer_template.tex -V theme=Warsaw
 
+@echo off
+Exit /B
+
 :END
 echo No input file.
 echo usage: makedoc mdfile
